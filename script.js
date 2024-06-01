cb3e87d37292dd1adcaeedff56492968711e6eb8
Nitish Jaiswal <141999440+nitishjaiswal2002@users.noreply.github.com>
2024-06-01 18:26:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+// script.js
+// script.js
+document.addEventListener("DOMContentLoaded", function() {
+  const checkboxes = document.querySelectorAll('.toggle');
+  
+  checkboxes.forEach(checkbox => {
+    checkbox.addEventListener('change', function() {
+      const checked = document.querySelectorAll('.toggle:checked');
+      if (checked.length > 2) {
+        this.checked = false;
+      }
+    });
+
+    // Toggle color on click
+    const span = checkbox.nextElementSibling;
+    span.addEventListener('click', () => {
+      checkbox.checked = !checkbox.checked;
+      checkbox.dispatchEvent(new Event('change'));
+    });
+  });
+});
